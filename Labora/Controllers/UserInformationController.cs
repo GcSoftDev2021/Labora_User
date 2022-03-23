8fa227bdc5ceb9430342610055be2d4c8d5f1b83
oscar.castillo <oscar.castillo@LPTOCASTILLO>
2022-03-23 16:03:12 -0500

    Information Personal

---
+++ Labora/Controllers/UserInformationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labora.Controllers
+{
+    public class UserInformationController : Controller
+    {
+        // GET: UserInformation
+        public ActionResult PersonalInformation()
+        {
+            return View();
+        }
+    }
+}
