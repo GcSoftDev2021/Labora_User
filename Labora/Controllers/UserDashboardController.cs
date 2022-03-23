c64410e3dbe95229374f23702addd39edc2b20eb
Oscar Castillo <OscarCastillo@CastilloGarzon.onmicrosoft.com>
2022-03-22 20:21:39 -0500

    users dashboard

---
+++ Labora/Controllers/UserDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labora.Controllers
+{
+    public class UserDashboardController : Controller
+    {
+        // GET: UserDashboard
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
